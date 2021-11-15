8898573aef071a7a97126874a3fb4ff19abcd797
chench <changhua.chen@qcast.cn>
2021-11-15 17:53:36 +0800

    first

---
+++ Theory of Computation.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Theory of Computation" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1636712489307">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="introduciton of TOC and DFA()" POSITION="right" ID="ID_75641125" CREATED="1636712493605" MODIFIED="1636712560332">
+<node TEXT="TOC" ID="ID_475260208" CREATED="1636712560682" MODIFIED="1636712562962">
+<node TEXT="什么理论" ID="ID_729739562" CREATED="1636712680693" MODIFIED="1636712688325">
+<node TEXT="计算机器" ID="ID_444391061" CREATED="1636712577724" MODIFIED="1636712739695">
+<node TEXT="能力" ID="ID_1943409136" CREATED="1636712744205" MODIFIED="1636712755005"/>
+<node TEXT="能解决什么问题" ID="ID_556355994" CREATED="1636712749280" MODIFIED="1636712781563"/>
+<node TEXT="限制" ID="ID_576919263" CREATED="1636712781900" MODIFIED="1636712789408"/>
+</node>
+</node>
+<node TEXT="概念" ID="ID_328575820" CREATED="1636958986377" MODIFIED="1636959005880">
+<node TEXT="symbol" ID="ID_201910106" CREATED="1636712796067" MODIFIED="1636712803393">
+<node TEXT="anything" ID="ID_1428269828" CREATED="1636713016614" MODIFIED="1636713020982">
+<node TEXT="a,b" ID="ID_910040803" CREATED="1636967969774" MODIFIED="1636967972844"/>
+</node>
+</node>
+<node TEXT="alphabet" ID="ID_402187976" CREATED="1636712851224" MODIFIED="1636712858569">
+<node TEXT="a set of symbol" ID="ID_296515875" CREATED="1636712970158" MODIFIED="1636712975760">
+<node TEXT="有限的" ID="ID_1062498840" CREATED="1636969027758" MODIFIED="1636969031863"/>
+<node TEXT="\latex \Sigma=\{a, b\}" ID="ID_1467726369" CREATED="1636967846196" MODIFIED="1636967989406"/>
+</node>
+<node TEXT="power of sigma" ID="ID_1966663219" CREATED="1636968409994" MODIFIED="1636968419433">
+<node TEXT="\latex \Sigma^2" ID="ID_1502253175" CREATED="1636968959224" MODIFIED="1636968967902"/>
+<node TEXT="集合的笛卡尔积，对alphabet，n次方就是长度为n的所有string的集合" ID="ID_1363533883" CREATED="1636968420211" MODIFIED="1636968467253"/>
+</node>
+<node TEXT="\latex \Sigma^*=\Sigma^0\cup\Sigma^1..." ID="ID_662366222" CREATED="1636968688130" MODIFIED="1636968761621">
+<node TEXT="字母表组成的所有string的集合" ID="ID_1958766210" CREATED="1636968805093" MODIFIED="1636968823627"/>
+</node>
+</node>
+<node TEXT="string" ID="ID_607097422" CREATED="1636712976748" MODIFIED="1636712977883">
+<node TEXT="a sequence of symbol in alphabet" ID="ID_1322996277" CREATED="1636712978673" MODIFIED="1636712999747"/>
+<node TEXT="长度为n的string的所有可能" ID="ID_1214924015" CREATED="1636958868456" MODIFIED="1636958885535">
+<node TEXT="\latex |\Sigma|^n" ID="ID_920055472" CREATED="1636967939491" MODIFIED="1636967954050"/>
+</node>
+</node>
+<node TEXT="language" ID="ID_516848518" CREATED="1636967807458" MODIFIED="1636967810553">
+<node TEXT="collection of strings" ID="ID_1497071968" CREATED="1636967810825" MODIFIED="1636967815108">
+<node TEXT="长度为2的所有string, {aa, ab, ba, bb}" ID="ID_1315624251" CREATED="1636967999303" MODIFIED="1636968021075"/>
+</node>
+<node TEXT="有限/无限" ID="ID_691723532" CREATED="1636968082974" MODIFIED="1636968097060"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ typescript.mm
@@ -0,0 +1,105 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="typescript" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1631874304513">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="问题" POSITION="right" ID="ID_1268442906" CREATED="1631516904540" MODIFIED="1631874315876">
+<node TEXT="泛型" ID="ID_1607565349" CREATED="1631874316570" MODIFIED="1631874322596">
+<node TEXT="在泛型约束中使用类型参数" ID="ID_1098685960" CREATED="1631874326216" MODIFIED="1631874355465">
+<node ID="ID_1134334432" CREATED="1631874736418" MODIFIED="1631874795179"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span class="hljs-keyword" style="font-size: 1em !important; line-height: 1.6em; color: rgb(0, 0, 255)"><font size="1em !important" color="rgb(0, 0, 255)" face="Menlo, Monaco, Courier New, monospace">function</font></span><span charset="utf-8" class="hljs-function" style="font-size: 16px; line-height: 1.6em; color: rgb(47, 79, 79); font-family: Menlo, Monaco, Courier New, monospace; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: pre-wrap; word-spacing: 0px"><font size="16px" color="rgb(47, 79, 79)" face="Menlo, Monaco, Courier New, monospace">&nbsp;</font></span><span class="hljs-title" style="font-size: 1em !important; line-height: 1.6em; color: rgb(163, 21, 21)"><font size="1em !important" color="rgb(163, 21, 21)" face="Menlo, Monaco, Courier New, monospace">getProperty</font></span><font color="rgb(47, 79, 79)" face="Menlo, Monaco, Courier New, monospace" size="16px"><t k="" extends="" keyof="" t="" align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: pre-wrap; word-spacing: 0px"></font><span class="hljs-function" style="font-size: 1em !important; line-height: 1.6em"><font size="1em !important">(</font></span><font size="1em !important"><span class="hljs-params" style="font-size: 1em !important; line-height: 1.6em">obj: T, key: K</span><span class="hljs-function" style="font-size: 1em !important; line-height: 1.6em">) </span></font>{ <span class="hljs-keyword" style="font-size: 1em !important; line-height: 1.6em; color: rgb(0, 0, 255)"><font size="1em !important" color="rgb(0, 0, 255)">return</font></span>&nbsp;obj[key]; } <span class="hljs-keyword" style="font-size: 1em !important; line-height: 1.6em; color: rgb(0, 0, 255)"><font size="1em !important" color="rgb(0, 0, 255)">let</font></span>&nbsp;x = { a: <span class="hljs-number" style="font-size: 1em !important; line-height: 1.6em"><font size="1em !important">1</font></span>, b: <span class="hljs-number" style="font-size: 1em !important; line-height: 1.6em"><font size="1em !important">2</font></span>, c: <span class="hljs-number" style="font-size: 1em !important; line-height: 1.6em"><font size="1em !important">3</font></span>, d: <span class="hljs-number" style="font-size: 1em !important; line-height: 1.6em"><font size="1em !important">4</font></span>&nbsp;}; &nbsp;getProperty(x, <span class="hljs-string" style="font-size: 1em !important; line-height: 1.6em; color: rgb(163, 21, 21)"><font size="1em !important" color="rgb(163, 21, 21)">&quot;a&quot;</font></span>); <span class="hljs-comment" style="font-size: 1em !important; line-height: 1.6em; color: rgb(0, 128, 0)"><font size="1em !important" color="rgb(0, 128, 0)">// okay</font></span>&nbsp;&nbsp;getProperty(x, <span class="hljs-string" style="font-size: 1em !important; line-height: 1.6em; color: rgb(163, 21, 21)"><font size="1em !important" color="rgb(163, 21, 21)">&quot;m&quot;</font></span>); <span class="hljs-comment" style="font-size: 1em !important; line-height: 1.6em; color: rgb(0, 128, 0)"><font size="1em !important" color="rgb(0, 128, 0)">// error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.</font></span></t>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="在泛型里使用类类型" ID="ID_1958185610" CREATED="1631874839657" MODIFIED="1631874849778">
+<node TEXT="function create&lt;T&gt;(c: {new(): T; }): T {&#xa;    return new c();&#xa;}" ID="ID_482182482" CREATED="1631874857823" MODIFIED="1631874859427"/>
+</node>
+</node>
+</node>
+</node>
+</map>
